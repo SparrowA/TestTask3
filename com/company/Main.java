d2dce761fe4f1ec3d23c0ad8b90bf848e42407c2
SparrowDev <45_93@mail.ru>
2016-08-18 18:10:34 +0300

    Create rep. to work continuation of home

---
+++ com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static  boolean WriteFile(File in, File out){
+
+    }
+}
